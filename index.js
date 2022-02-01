3e2f5b3102e683acd17ad29ef17f8398a770010e
feh-franc0 <fernandofv1110@gmail.com>
2022-02-01 11:23:07 -0300

    Express - GET

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ const app = express()//recebe nosso objeto express
 //xxx.xxx.x.xx:5000/meusite/
 app.use("/meusite",express.static(path.join(__dirname,'client')))
 
+app.get("/", (req,res) => {
+    // res.set("Content-Type", tet/html);
+    // res.type('.html') => 'text/html'
+    // res.type('html') => 'text/html'
+    // res.type('txt') => 'text/plain'
+    // res.type('json') => 'application/json'
+    // res.type('application/json') => 'application/json'
+    // res.type('png') => 'image/png'
+    res.send("<h1>Hello World From GET</h1>"); 
+})
+
 const PORT = 5000;
 app.listen(PORT, ()=>{
     console.log(`Server Runing on Port:${PORT}`)
