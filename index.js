52ce30370cd76220941d0d62260f1427212f2a3c
feh-franc0 <fernandofv1110@gmail.com>
2022-02-01 10:48:07 -0300

    Arquivos estátics com Express

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 //npm install express
 
 const express = require('express');
+const path = require('path') //ajuda a montar o caminho dos nossos arquivos,modulo padrao do node
+// Servir arquivos estaticos utilizando o express
 
 const app = express()//recebe nosso objeto express
+
+//xxx.xxx.x.xx:5000/meusite/
+app.use("/meusite",express.static(path.join(__dirname,'client')))
 
 const PORT = 5000;
 app.listen(PORT, ()=>{
