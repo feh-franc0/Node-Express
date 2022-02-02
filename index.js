c947ba1c6fc55f021bb00deef43f3d7e54ce426b
feh-franc0 <fernandofv1110@gmail.com>
2022-02-02 00:42:45 -0300

    Rotas e formas de passar dados pela requisição

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 var express = require('express');
-var path = require('path');
-var bodyParser = require("body-parser");//temos acesso ao conteeudo da body da nossa requisiçao
+var app = express();
+var bodyParser = require("body-parser");
 
-const app = express()
+let alunos = [{ id: 0, nome: "Jose" },
+{ id: 1, nome: "Maria" },
+{ id: 2, nome: "Joao" },
+{ id: 3, nome: "Marcos" }]
 
 
-app.use("/meusite",express.static(path.join(__dirname,'client')))
+app.use(bodyParser.urlencoded())
 
-let consoleBody = (req, res, next) => {
-    // console.log(req.method);
-    console.log(req.body);
-    next()
-}
 
-let middleware = (req,res) => {
-    res.send("Hello World"); 
-}
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
 
-app.use("/", bodyParser.json())
-//use é o metodo que a gente ultiliza para o meddleware funcionar em todos os metodos
-app.use("/", consoleBody) 
+app.get("/alunos", (req, res) => {
+    res.json(JSON.stringify(alunos))
+})
 
-// pode usar assim
-// app.get("/", consoleMethod)
-app.get("/", middleware)
-// ou assim 
-// app.post("/", consoleMethod, middleware)
-app.post("/", middleware)
-// app.put("/", consoleMethod, middleware)
-app.put("/", middleware)
-// app.delete("/", consoleMethod, middleware)
-app.delete("/", middleware)
 
-app.listen(5000, () => console.log('Server rodando...'))
+// Passar dados pela requisição:
 
-// const PORT = 5000;
-// app.listen(PORT, ()=>{
-//     console.log(`Server Runing on Port:${PORT}`)
+// app.get("/aluno", (req,res) => {
+//     console.log(req.body);
+//     let aluno = alunos[req.body.id]
+//     res.json(aluno);
 // })
 
+app.get("/aluno", (req,res) => {
+    console.log(req.query);
+    let aluno = alunos[req.query.id]
+    res.json(aluno);
+})
+
+// app.get("/aluno/:id", (req,res) => {
+//     console.log(req.params.id);
+//     let aluno = alunos[req.params.id]
+//     res.json(aluno);
+// })
+
+app.listen(5000, () => console.log('Server rodando...'))
