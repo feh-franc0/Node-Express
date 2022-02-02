a34c9f569cf6b0f59a6ce14ed29a6c44c1a75870
feh-franc0 <fernandofv1110@gmail.com>
2022-02-02 00:16:36 -0300

    Aprendendo: use,middleware e body-parser

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-//npm init -y
-//npm install express
+var express = require('express');
+var path = require('path');
+var bodyParser = require("body-parser");//temos acesso ao conteeudo da body da nossa requisiçao
 
-const express = require('express');
-const path = require('path') //ajuda a montar o caminho dos nossos arquivos,modulo padrao do node
-// Servir arquivos estaticos utilizando o express
+const app = express()
 
-const app = express()//recebe nosso objeto express
 
-//xxx.xxx.x.xx:5000/meusite/
 app.use("/meusite",express.static(path.join(__dirname,'client')))
 
-app.get("/", (req,res) => {
-    // res.set("Content-Type", tet/html);
-    // res.type('.html') => 'text/html'
-    // res.type('html') => 'text/html'
-    // res.type('txt') => 'text/plain'
-    // res.type('json') => 'application/json'
-    // res.type('application/json') => 'application/json'
-    // res.type('png') => 'image/png'
-    res.send("<h1>Hello World From GET</h1>"); 
-})
+let consoleBody = (req, res, next) => {
+    // console.log(req.method);
+    console.log(req.body);
+    next()
+}
 
-const PORT = 5000;
-app.listen(PORT, ()=>{
-    console.log(`Server Runing on Port:${PORT}`)
-})//escuta as requisisoes http
+let middleware = (req,res) => {
+    res.send("Hello World"); 
+}
 
-//terminal: ipconfig
-//ache o Wireless LAN adapter Wi-Fi>IPv4 adress> xxx.xxx.x.xx
-// +> a maquina que estamos usando tem esse endereço "XXX.XXX.X.XX"
+app.use("/", bodyParser.json())
+//use é o metodo que a gente ultiliza para o meddleware funcionar em todos os metodos
+app.use("/", consoleBody) 
+
+// pode usar assim
+// app.get("/", consoleMethod)
+app.get("/", middleware)
+// ou assim 
+// app.post("/", consoleMethod, middleware)
+app.post("/", middleware)
+// app.put("/", consoleMethod, middleware)
+app.put("/", middleware)
+// app.delete("/", consoleMethod, middleware)
+app.delete("/", middleware)
+
+app.listen(5000, () => console.log('Server rodando...'))
+
+// const PORT = 5000;
+// app.listen(PORT, ()=>{
+//     console.log(`Server Runing on Port:${PORT}`)
+// })
+
